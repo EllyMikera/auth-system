47cf534eb2aac2ffbbf0d94b1c8c32f1f93e3bab
Elly <mikeraelly015@gmail.com>
2026-07-08 13:13:49 +0300

    romoved a log statement

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -16,7 +16,6 @@ async function handleLogin() {
     const startTime = Date.now()
     try{
         event.preventDefault();
-        console.log("Api url: " + import.meta.env.VITE_API_URL);
         const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
             method: "POST",
             headers: {
